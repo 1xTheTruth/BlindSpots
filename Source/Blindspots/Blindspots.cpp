b638a6a4fc9ce711c3b7d577787bf4164cd219fb
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-09-19 17:36:04 +0300

    Initial Project Files

---
+++ Source/Blindspots/Blindspots.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Blindspots.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Blindspots, "Blindspots" );
