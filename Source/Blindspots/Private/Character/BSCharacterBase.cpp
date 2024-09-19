b7eac00ea2c7a8103bb48979013ca793a1a70d2f
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-09-19 19:00:27 +0300

    Character Blueprint Setup

--- Source/Blindspots/Private/Character/BSCharacterBase.cpp
+++ Source/Blindspots/Private/Character/BSCharacterBase.cpp
@@ -6,6 +6,10 @@
 ABSCharacterBase::ABSCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ABSCharacterBase::BeginPlay()
