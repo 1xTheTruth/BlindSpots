81a403d89d469f6959ad4e414d3102ac14db846b
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-09-20 06:06:05 +0300

    Player Controller

--- Source/Blindspots/Private/Player/BSPlayerController.cpp
+++ Source/Blindspots/Private/Player/BSPlayerController.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Player/BSPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ABSPlayerController::ABSPlayerController()
+{
+	bReplicates = true;
+}
+
+void ABSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(BSContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(BSContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/Blindspots/Public/Player/BSPlayerController.h
+++ Source/Blindspots/Public/Player/BSPlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "BSPlayerController.generated.h"
 
+
+class UInputMappingContext;
 /**
  * 
  */
@@ -14,4 +16,16 @@ class BLINDSPOTS_API ABSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+	ABSPlayerController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> BSContext;
 };
