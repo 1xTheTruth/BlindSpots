d6590c2e59e9250545171a1beaf1d360548bff55
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-10-04 04:39:26 +0300

    Health Mana and Stamina

--- Source/Blindspots/Private/AbilitySystem/BSAttributeSet.cpp
+++ Source/Blindspots/Private/AbilitySystem/BSAttributeSet.cpp
@@ -2,4 +2,51 @@
 
 
 #include "AbilitySystem/BSAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UBSAttributeSet::UBSAttributeSet()
+{
+}
+
+void UBSAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, Mana, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, MaxMana, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, Stamina, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(UBSAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always)
+}
+
+void UBSAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, Health, OldHealth);
+}
+
+void UBSAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UBSAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, Mana, OldMana);
+}
+
+void UBSAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, MaxMana, OldMaxMana);
+}
+
+void UBSAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, Stamina, OldStamina);
+}
+
+void UBSAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBSAttributeSet, MaxStamina, OldMaxStamina)
+}
