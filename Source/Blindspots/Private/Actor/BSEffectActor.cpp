432d5df361dd57b91e3ed62ab0e013c466f37119
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-10-04 05:36:17 +0300

    Effect Actor

---
+++ Source/Blindspots/Private/Actor/BSEffectActor.cpp
@@ -0,0 +1,50 @@
+// Copyright Y.M. Bilge
+
+
+#include "Actor/BSEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/BSAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+ABSEffectActor::ABSEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+void ABSEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast because i'm lazy! 
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UBSAttributeSet* BSAttributeSet = Cast<UBSAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UBSAttributeSet::StaticClass()));
+		
+		UBSAttributeSet* MutableBSAttributeSet = const_cast<UBSAttributeSet*>(BSAttributeSet);
+		MutableBSAttributeSet->SetHealth(BSAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void ABSEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
+
+void ABSEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ABSEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &ABSEffectActor::EndOverlap);
+}
+
+
---
+++ Source/Blindspots/Public/Actor/BSEffectActor.h
@@ -0,0 +1,39 @@
+// Copyright Y.M. Bilge
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BSEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class BLINDSPOTS_API ABSEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	ABSEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
