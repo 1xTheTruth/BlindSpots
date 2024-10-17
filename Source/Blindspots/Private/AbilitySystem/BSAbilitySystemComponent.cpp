ed0358c9967ed838334b0769f8ba9f4a5c85d196
yagizbilge <113250404+1xTheTruth@users.noreply.github.com>
2024-10-17 16:29:08 +0300

    Get All Asset Tags

--- Source/Blindspots/Private/AbilitySystem/BSAbilitySystemComponent.cpp
+++ Source/Blindspots/Private/AbilitySystem/BSAbilitySystemComponent.cpp
@@ -11,5 +11,15 @@ void UBSAbilitySystemComponent::AbilityActorInfoSet()
 void UBSAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                               const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1,8.f,FColor::Blue,FString("EffectApplied"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		
+		//TODO: Broadcast the tag to the Widget Controller
+
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+		
+	}
 }
